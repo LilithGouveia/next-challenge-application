5905592b84b0da20c0268f09469b5852b6571b7d
Lilith Gouveia <lilithgouveia@outlook.com>
2023-10-12 14:29:35 -0300

    Login funcional [front]

--- src/components/FormLogin.jsx
+++ src/components/FormLogin.jsx
@@ -1,131 +1,49 @@
 import { Link } from 'react-router-dom';
-import '../style/formLogin.scss'
-import * as mdb from 'mdb-ui-kit'; // lib
-import { Input } from 'mdb-ui-kit'; // module
+import '../style/formLogin.scss';
 export default function Formlogin() {
   return (
     <>
-      <section className='vh-100'>
-        <div className='container-fluid h-custom'>
-          <div className='row d-flex justify-content-center align-items-center h-100'>
-            <div className='col-md-9 col-lg-6 col-xl-5'>
-              <img
-                src='https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp'
-                className='img-fluid'
-                alt='Sample image'
-              />
-            </div>
-            <div className='col-md-8 col-lg-6 col-xl-4 offset-xl-1'>
-              <form>
-                <div className='d-flex flex-row align-items-center justify-content-center justify-content-lg-start'>
-                  <p className='lead fw-normal mb-0 me-3'>Sign in with</p>
-                  <button
-                    type='button'
-                    className='btn btn-primary btn-floating mx-1'
-                  >
-                    <i className='fab fa-facebook-f'></i>
-                  </button>
-
-                  <button
-                    type='button'
-                    className='btn btn-primary btn-floating mx-1'
-                  >
-                    <i className='fab fa-twitter'></i>
-                  </button>
-
-                  <button
-                    type='button'
-                    className='btn btn-primary btn-floating mx-1'
-                  >
-                    <i className='fab fa-linkedin-in'></i>
-                  </button>
-                </div>
-
-                <div className='divider d-flex align-items-center my-4'>
-                  <p className='text-center fw-bold mx-3 mb-0'>Or</p>
-                </div>
-
-                {/* <!-- Email input --> */}
-                <div className='form-outline mb-4'>
-                  <input
-                    type='email'
-                    id='form3Example3'
-                    className='form-control form-control-lg'
-                    placeholder='Enter Link valid email address'
-                  />
-                  <label className='form-label' htmlFor='form3Example3'>
-                    Email address
-                  </label>
-                </div>
-
-                {/* <!-- Password input --> */}
-                <div className='form-outline mb-3'>
-                  <input
-                    type='password'
-                    id='form3Example4'
-                    className='form-control form-control-lg'
-                    placeholder='Enter password'
-                  />
-                  <label className='form-label' htmlFor='form3Example4'>
-                    Password
-                  </label>
-                </div>
-
-                <div className='d-flex justify-content-between align-items-center'>
-                  {/* <!-- Checkbox --> */}
-                  <div className='form-check mb-0'>
+      <body>
+        <div className='wrapper'>
+          <div className='container main'>
+            <div className='row'>
+              <div className='col-md-6 side-image'/>
+              <div className='col-md-6 right'>
+                <div className='input-box'>
+                  <header>Create account</header>
+                  <div className='input-field'>
                     <input
-                      className='form-check-input me-2'
-                      type='checkbox'
-                      value=''
-                      id='form2Example3'
+                      type='text'
+                      className='input'
+                      id='email'
+                      required=''
+                      autoComplete='off'
                     />
-                    <label className='form-check-label' htmlFor='form2Example3'>
-                      Remember me
-                    </label>
+                    <label htmlFor='email'>Email</label>
                   </div>
-                  <Link href='#!' className='text-body'>
-                    Forgot password?
-                  </Link>
+                  <div className='input-field'>
+                    <input
+                      type='password'
+                      className='input'
+                      id='pass'
+                      required=''
+                    />
+                    <label htmlFor='pass'>Password</label>
+                  </div>
+                  <div className='input-field'>
+                    <input type='submit' className='submit' value='Sign Up' />
+                  </div>
+                  <div className='signin'>
+                    <span>
+                      Already have an account? <Link href='#'>Log in here</Link>
+                    </span>
+                  </div>
                 </div>
-
-                <div className='text-center text-lg-start mt-4 pt-2'>
-                  <button
-                    type='button'
-                    className='btn btn-primary btn-lg'
-                  >
-                    Login
-                  </button>
-                  <p className='small fw-bold mt-2 pt-1 mb-0'>
-                    Sem conta?{' '}
-                    <Link href='#!' className='link-danger'>
-                      Register
-                    </Link>
-                  </p>
-                </div>
-              </form>
+              </div>
             </div>
           </div>
         </div>
-        <div className='d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary'>
-          {/* <!-- Right --> */}
-          <div>
-            <Link href='#!' className='text-white me-4'>
-              <i className='fab fa-facebook-f'></i>
-            </Link>
-            <Link href='#!' className='text-white me-4'>
-              <i className='fab fa-twitter'></i>
-            </Link>
-            <Link href='#!' className='text-white me-4'>
-              <i className='fab fa-google'></i>
-            </Link>
-            <Link href='#!' className='text-white'>
-              <i className='fab fa-linkedin-in'></i>
-            </Link>
-          </div>
-          {/* <!-- Right --> */}
-        </div>
-      </section>
+      </body>
     </>
   );
 }
