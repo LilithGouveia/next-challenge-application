bd41a5921f905d53e4cce50ff7fc8d0a2f7b7684
tomaz versolato carballo <RM551417@fiap.com.br>
2023-10-12 18:28:22 -0300

    Começando DashBoard.

--- src/components/HeaderMain.jsx
+++ src/components/HeaderMain.jsx
@@ -1,18 +1,20 @@
-import AquaLogo from '../assets/aquatank_logo.png';
-import { Link } from 'react-router-dom';
-import '../style/headerMain.scss';
+import AquaLogo from "../assets/aquatank_logo.png";
+import { Link } from "react-router-dom";
+import "../style/headerMain.scss";
 
 export default function HeaderMain() {
   return (
     <>
-      <nav className='navbar navbar-expand-lg'>
-        <div className='container-fluid'>
-          <Link className='navbar-brand' to='/'>
-            <img src={AquaLogo} alt='logo' className='img-logo' />
+      <nav className="navbar navbar-expand-lg">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            <img src={AquaLogo} alt="logo" className="img-logo" />
           </Link>
-            <ul className='navbar-nav me-auto mb-2 mb-lg-0'></ul>
-            <Link to='/login'>Login</Link>
-          </div>
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0"></ul>
+          <Link className="loginBotao" to="/login">
+            Login
+          </Link>
+        </div>
       </nav>
     </>
   );
--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,4 +1,5 @@
 import MainImage from '../assets/lukevrogers_A_technological_tree_made_out_of_cyan_nodes_9a8c31ad-6025-4b17-947a-c0d3fe83b979.png';
+import Teste from "../assets/teste.png";
 import '../style/mainContent.scss';
 
 export default function MainContent() {
@@ -15,7 +16,7 @@ export default function MainContent() {
             Junte-se a iniciativa capaz de acabar com isso!
           </p>
         </div>
-        <img src={MainImage} className='image-main'/>
+        <img src={Teste} className='image-main'/>
       </div>
     </>
   );
