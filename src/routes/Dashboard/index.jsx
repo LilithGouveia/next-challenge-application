ad76577cac3ce48a811a58f22f18cd535be749e2
tomaz versolato carballo <RM551417@fiap.com.br>
2023-10-25 09:19:39 -0300

    Dash 80%

--- src/routes/Dashboard/index.jsx
+++ src/routes/Dashboard/index.jsx
@@ -3,21 +3,19 @@ import ReactApexChart from 'react-apexcharts';
 import "../../style/dashboard.scss";
 
 function ChartComponent() {
-  const [data, setData] = useState([]);
+  const [dataC, setDataC] = useState([]);
+  const [dataV, setDataV] = useState([]);
+  const [dataT, setDataT] = useState([]);
+  const [dataH, setDataH] = useState([]);
+  const [dataL, setDataL] = useState([]);
 
-  const fetchData = () => {
-    // Fazer a solicitação à API
-    fetch('http://localhost:3000/c')
+  const fetchData = (url, setData) => {
+    fetch(url)
       .then((response) => response.json())
       .then((responseData) => {
-        // Verificar se responseData contém a matriz contextResponses
         if (Array.isArray(responseData.contextResponses)) {
-          // Acessar a matriz de atributos
           const attributes = responseData.contextResponses[0].contextElement.attributes;
-
-          // Verificar se attributes é uma matriz e não está vazia
           if (Array.isArray(attributes) && attributes.length > 0) {
-            // Extrair os dados necessários para o gráfico
             const chartData = attributes[0].values.map((item) => ({
               x: new Date(item.recvTime).getTime(),
               y: item.attrValue,
@@ -36,37 +34,73 @@ function ChartComponent() {
   };
 
   useEffect(() => {
-    // Inicialmente, carregue os dados do gráfico
-    fetchData();
+    // Inicialmente, carregue os dados de todos os gráficos
+    fetchData('http://localhost:3000/c', setDataC);
+    fetchData('http://localhost:3000/v', setDataV);
+    fetchData('http://localhost:3000/t', setDataT);
+    fetchData('http://localhost:3000/h', setDataH);
+    fetchData('http://localhost:3000/l', setDataL);
 
-    // Em seguida, configure um intervalo para atualizar os dados do gráfico a cada X milissegundos (por exemplo, a cada 5 segundos)
+    // Em seguida, configure um intervalo para atualizar os dados dos gráficos a cada X milissegundos (por exemplo, a cada 5 segundos)
     const interval = setInterval(() => {
-      fetchData();
+      fetchData('http://localhost:3000/c', setDataC);
+      fetchData('http://localhost:3000/v', setDataV);
+      fetchData('http://localhost:3000/t', setDataT);
+      fetchData('http://localhost:3000/h', setDataH);
+      fetchData('http://localhost:3000/l', setDataL);
     }, 5000); // Atualize a cada 5 segundos (ajuste conforme necessário)
 
     // Lembre-se de limpar o intervalo quando o componente for desmontado para evitar vazamentos de memória
     return () => clearInterval(interval);
   }, []);
 
-  const options = {
+  const chartOptions = {
     chart: {
-      id: 'basic-bar',
+      id: 'basic-line-chart',
+      toolbar: {
+        show: true, // Exibir a barra de ferramentas do gráfico
+        tools: {
+          zoom: true, // Habilitar a ferramenta de zoom
+          pan: true, // Habilitar a ferramenta de pan
+          reset: true, // Habilitar a ferramenta de reset (voltar ao estado inicial)
+        },
+      },
+      zoom: {
+        enabled: true, // Habilitar o zoom
+      },
     },
     xaxis: {
       type: 'datetime',
     },
   };
 
-  const series = [
-    {
-      name: 'Valores',
-      data: data,
-    },
-  ];
-
   return (
-    <div className="chart">
-      <ReactApexChart options={options} series={series} type="line" height={350} />
+    <div className="chart-container">
+      <h1 className='titulo'>Overview</h1>
+      <div className="top-row">
+        <div className="chart">
+          <div className="chart-label">CO2</div>
+          <ReactApexChart options={chartOptions} series={[{ name: 'CO2', data: dataC }]} type="line" height={350} />
+        </div>
+        <div className="chart">
+          <div className="chart-label">TVOC</div>
+          <ReactApexChart options={chartOptions} series={[{ name: 'TVOC', data: dataV }]} type="line" height={350} />
+        </div>
+      </div>
+      <div className="bottom-row">
+        <div className="chart">
+          <div className="chart-label">Temperatura</div>
+          <ReactApexChart options={chartOptions} series={[{ name: 'Temperatura', data: dataT }]} type="line" height={350} />
+        </div>
+        <div className="chart">
+          <div className="chart-label">Umidade</div>
+          <ReactApexChart options={chartOptions} series={[{ name: 'Umidade', data: dataH }]} type="line" height={350} />
+        </div>
+      </div>
+      <div className="chart-bottom">
+        <div className="chart-label">Luminosidade</div>
+        <ReactApexChart options={chartOptions} series={[{ name: 'Luminosidade', data: dataL }]} type="line" height={350} />
+      </div>
     </div>
   );
 }
