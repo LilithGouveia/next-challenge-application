3a5bd5c0d58b62397cbaead8fddc5527b3573813
gugow06 <gustavo@phoenyx.com.br>
2023-11-07 18:42:51 -0300

    sessionStorage feito

--- src/components/FormLogin.jsx
+++ src/components/FormLogin.jsx
@@ -1,13 +1,35 @@
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import '../style/formLogin.scss';
+
 export default function FormLogin() {
+  const [formData, setFormData] = useState({ email: '', password: '' });
+
+  // Use useEffect to retrieve data from sessionStorage when the component mounts
+  useEffect(() => {
+    const storedData = sessionStorage.getItem('loginFormData');
+    if (storedData) {
+      const parsedData = JSON.parse(storedData);
+      setFormData(parsedData);
+    }
+  }, []);
+
+  const handleFormChange = (e) => {
+    const { id, value } = e.target;
+    const newFormData = { ...formData, [id]: value };
+    setFormData(newFormData);
+
+    // Store the form data in sessionStorage
+    sessionStorage.setItem('loginFormData', JSON.stringify(newFormData));
+  };
+
   return (
     <>
       <body>
         <div className='wrapper'>
           <div className='container main'>
             <div className='row'>
-              <div className='col-md-6 side-image'/>
+              <div className='col-md-6 side-image' />
               <div className='col-md-6 right'>
                 <div className='input-box'>
                   <header>Logue aqui</header>
@@ -18,6 +40,8 @@ export default function FormLogin() {
                       id='email'
                       required
                       autoComplete='off'
+                      value={formData.email}
+                      onChange={handleFormChange}
                     />
                     <label htmlFor='email'>E-mail</label>
                   </div>
@@ -25,17 +49,21 @@ export default function FormLogin() {
                     <input
                       type='password'
                       className='input'
-                      id='pass'
+                      id='password'
                       required
+                      value={formData.password}
+                      onChange={handleFormChange}
                     />
-                    <label htmlFor='pass'>Senha</label>
+                    <label htmlFor='password'>Senha</label>
                   </div>
                   <div className='input-field'>
+                    <Link className="submit"to="/dashboard">
                     <input type='submit' className='submit' value='Entrar' />
+                    </Link>
                   </div>
                   <div className='signin'>
                     <span>
-                      Não tem uma conta? <Link href='#'>Inscreva-se</Link>
+                      Não tem uma conta? <Link to='#'>Inscreva-se</Link>
                     </span>
                   </div>
                 </div>
