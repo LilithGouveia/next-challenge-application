d92df19211f5c586b74895934ff80a48b07d4022
Rony Ken <ronyknagai@gmail.com>
2023-11-11 16:43:55 -0300

    colocando dropdown

--- src/components/HeaderAboutUs.jsx
+++ src/components/HeaderAboutUs.jsx
@@ -1,5 +1,6 @@
 import AquaLogo from "../assets/aquatank_logo.png";
 import { Link } from "react-router-dom";
+import Dropdown from 'react-bootstrap/Dropdown';
 import "../style/headerAboutUs.scss";
 
 export default function HeaderAboutUs() {
@@ -11,13 +12,15 @@ export default function HeaderAboutUs() {
             <img src={AquaLogo} alt="logo" className="img-logo" />
           </Link>
           <ul className="navbar-nav me-auto mb-2 mb-lg-0"></ul>
-          <Link className="sobreNosBotao" to="/">
-            Home
-          </Link>
-          <h2>|</h2>
-          <Link className="loginBotao" to="/login">
-            Login 
-          </Link>
+          <Dropdown>
+            <Dropdown.Toggle variant="" id="dropdown-basic" className="loginbtn mt-2">Menu</Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item> <Link className="sobreNosBotao" to="/">Home</Link></Dropdown.Item>
+              <Dropdown.Item> <Link className="sobreNosBotao" to="/sobreNos">Sobre nós</Link></Dropdown.Item>
+              <Dropdown.Item> <Link className="loginBotao" to="/login">Login</Link></Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
         </div>
       </nav>
     </>
